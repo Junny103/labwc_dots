7cd08e9f9eb48baf01d71097ed03f2b5f4e5f509
Junny103 <cjzm@naver.com>
2026-02-10 02:18:52 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,20 +15,8 @@ sudo mkdir -p /usr/local/share/fonts/JetBrainsMono
 sudo unzip /tmp/JetBrainsMono.zip -d /usr/local/share/fonts/JetBrainsMono
 sudo fc-cache -fv
 
-# 3. Configure greetd with tuigreet
-echo "Configuring greetd with tuigreet..."
-sudo tee /etc/greetd/config.toml > /dev/null <<EOF
-[terminal]
-vt = 1
-
-[default_session]
-command = "tuigreet --asterisks --remember --cmd labwc"
-user = "greeter"
-EOF
-
-# 4. Set boot target to graphical and enable greetd service
+# 3. Set boot target to graphical and enable greetd service
 sudo systemctl set-default graphical.target
-sudo systemctl enable greetd
 
 echo "----------------------------------------------------"
 echo "Installation and system configuration complete!"
