be150b5e43056341ecee19c1587ec51736b7c0da
Junny103 <cjzm@naver.com>
2026-02-14 21:22:05 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ sudo apt install -y --no-install-recommends \
     labwc wlr-randr greetd tuigreet alacritty \
     thunar thunar-archive-plugin xarchiver \
     mousepad wl-clipboard fcitx5 fcitx5-hangul fonts-noto-cjk \
-    pipewire wireplumber pipewire-pulse \
+    pipewire wireplumber pipewire-pulse cava \
     waybar swaybg firefox-esr \
 
 # 2. Download and install JetBrainsMono Nerd Font
