bd89076a9a00c832ae206002335ea4ab7206ad5e
Junny103 <cjzm@naver.com>
2026-02-19 23:27:42 +0900

    Delete box64.sh

--- box64.sh
+++
@@ -1,9 +0,0 @@
-git clone https://github.com/ptitSeb/box64
-cd box64
-mkdir build && cd build
-
-cmake .. -D ARM_DYNAREC=ON -D RPI4ARM64=1 -D CMAKE_BUILD_TYPE=RelWithDebInfo -D BOX32=ON -D BOX32_BINFMT=ON
-make -j$(nproc)
-
-sudo make install
-sudo systemctl restart systemd-binfmt
