e1b692e3d37219cc4d8ea5a7abb58fb98d08932a
Junny103 <cjzm@naver.com>
2026-02-21 00:33:46 +0900

    Update and rename wine32.sh to wine.sh

--- games/wine.sh
+++ games/wine.sh
@@ -10,7 +10,24 @@ echo "2. Install build tools"
 sudo apt install -y \
 cmake gcc-arm-linux-gnueabihf \
 
-echo "3. Build and install box86"
+echo "3. Build and install box64"
+git clone https://github.com/ptitSeb/box64 || true
+cd box64
+mkdir -p build && cd build
+
+cmake .. \
+ -D ARM_DYNAREC=ON \
+ -D RPI4ARM64=1 \
+ -D CMAKE_BUILD_TYPE=RelWithDebInfo \
+ -D BOX32=ON \
+ -D BOX32_BINFMT=ON
+
+make -j$(nproc)
+sudo make install
+
+cd ../..
+
+echo "4. Build and install box86"
 git clone https://github.com/ptitSeb/box86 || true
 cd box86
 mkdir -p build && cd build
@@ -27,8 +44,16 @@ cd ../..
 echo "4. Restart binfmt"
 sudo systemctl restart systemd-binfmt
 
-echo "5. Install Wine32"
-sudo apt install -y wine32
+echo "5. Install Wine"
+sudo apt build-dep wine
+
+mkdir -p ~/wine
+cd ~/wine
+wget https://github.com/Kron4ek/Wine-Builds/releases/download/11.2/wine-11.2-amd64-wow64.tar.xz
+tar -xvf wine-11.2-amd64-wow64.tar.xz --strip-components=1
+
+echo 'export PATH="$HOME/wine/bin:$PATH"' >> ~/.bashrc
+source ~/.bashrc
 
 echo "----------------------------------------------------"
 echo "Installation complete!"
