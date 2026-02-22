cea6f9ae450d704d874ce8e74c09d3f17327022c
Junny103 <cjzm@naver.com>
2026-02-22 16:59:47 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,13 +14,23 @@ echo "2. Download themes and icons"
 git clone https://github.com/Fausto-Korpsvart/Catppuccin-GTK-Theme.git
 
 echo "3. Download and install JetBrainsMono Nerd Font"
-echo "Downloading and installing JetBrainsMono Nerd Font..."
 wget -P /tmp https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/JetBrainsMono.zip
 sudo mkdir -p /usr/local/share/fonts/JetBrainsMono
 sudo unzip /tmp/JetBrainsMono.zip -d /usr/local/share/fonts/JetBrainsMono
 sudo fc-cache -fv
 
-echo "4. Set boot target to graphical and enable greetd service"
+echo "4. Copy config files and directories"
+
+mkdir -p "$HOME/.config"
+
+cp -rT ./alacritty "$HOME/.config/alacritty"
+cp -rT ./labwc "$HOME/.config/labwc"
+cp -rT ./waybar "$HOME/.config/waybar"
+cp -rT ./xfce4 "$HOME/.config/xfce4"
+
+sudo cp -f ./config.toml /etc/greetd/config.toml
+
+echo "5. Set boot target to graphical and enable greetd service"
 sudo systemctl set-default graphical.target
 
 echo "----------------------------------------------------"
