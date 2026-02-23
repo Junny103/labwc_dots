3e37337330f172286580cb3d79197dbb05f69c8b
Junny103 <cjzm@naver.com>
2026-02-24 01:25:42 +0900

    Update and rename wine.sh to openmw_dep.sh

---
+++ games/openmw_dep.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+set -e
+
+echo "----------------------------------------------------"
+echo "1. Update system and install packages"
+echo "----------------------------------------------------"
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y \
+    cmake libfreetype-dev libqt6opengl6-dev \
+    libopenal-dev libavcodec-dev libyaml-cpp-dev \
+
+echo "----------------------------------------------------"
+echo "2. Build and install box64"
+echo "----------------------------------------------------"
+git clone https://github.com/MyGUI/mygui.git || true
+cd mygui
+git checkout MyGUI3.4.3
+
+mkdir build && cd build
+cmake .. \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DMYGUI_RENDERSYSTEM=1 \
+  -DMYGUI_BUILD_DEMOS=OFF \
+  -DMYGUI_BUILD_TOOLS=OFF \
+  -DMYGUI_BUILD_PLUGINS=OFF \
+  -DMYGUI_DONT_USE_OBSOLETE=ON
+
+make -j$(nproc)
+sudo make install
+sudo ldconfig
+
+echo "----------------------------------------------------"
+echo "3. Install PiKISS (optional)"
+echo "----------------------------------------------------"
+
+read -p "Do you want to install PiKISS? (y/n): " install_pikiss
+
+if [[ "$install_pikiss" == "y" || "$install_pikiss" == "Y" ]]; then
+    git clone https://github.com/jmcerrejon/PiKISS.git || true
+    cd PiKISS
+    chmod +x piKiss.sh
+    ./piKiss.sh
+else
+    echo "Skipping PiKISS installation."
+fi
+
+echo "----------------------------------------------------"
+echo "Installation and build complete!"
+echo "----------------------------------------------------"
